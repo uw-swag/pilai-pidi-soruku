12bfbc68727b12bc8d4f31457e13e91153a169ca
GitHub Action <action@github.com>
2021-07-03 14:18:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/noblemathews/crosssrcbuggy/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.noblemathews.crosssrcbuggy.services
+
+import com.github.noblemathews.crosssrcbuggy.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/noblemathews/crosssrcbuggy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/noblemathews/crosssrcbuggy/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.noblemathews.crosssrcbuggy.services
 
+import com.github.noblemathews.crosssrcbuggy.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
