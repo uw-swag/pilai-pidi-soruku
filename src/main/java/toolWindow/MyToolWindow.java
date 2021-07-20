838df23ba6d832737258d8b45e30fb6bc8397865
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-07-20 21:34:30 +0530

    V1 Plugin

--- src/main/java/toolWindow/MyToolWindow.java
+++ src/main/java/toolWindow/MyToolWindow.java
@@ -2,10 +2,14 @@
 
 package toolWindow;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.WindowManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.noble.Main;
 import com.noble.models.Encl_name_pos_tuple;
 
@@ -15,8 +19,12 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
-import java.util.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.*;
+
+import static com.intellij.psi.search.FilenameIndex.getFilesByName;
 
 public class MyToolWindow implements TreeSelectionListener {
 
@@ -24,7 +32,10 @@ public class MyToolWindow implements TreeSelectionListener {
   private JButton hideToolWindowButton;
   private JPanel myToolWindowContent;
   private JTree filesInConnection;
+  @SuppressWarnings("unused")
+  private JScrollPane scrollerWindow;
 
+  private Project activeProject = null;
   public MyToolWindow(ToolWindow toolWindow) {
     hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
     refreshToolWindowButton.addActionListener(e -> runSrcBuggy());
@@ -36,7 +47,6 @@ public class MyToolWindow implements TreeSelectionListener {
     String[] arguments = {"location"};
     String directory;
     Project[] projects = ProjectManager.getInstance().getOpenProjects();
-    Project activeProject = null;
     for (Project project : projects) {
       Window window = WindowManager.getInstance().suggestParentWindow(project);
       if (window != null && window.isActive()) {
@@ -49,26 +59,27 @@ public class MyToolWindow implements TreeSelectionListener {
 
     DefaultMutableTreeNode root = new DefaultMutableTreeNode("Possible Buffer Overflow Violations");
     DefaultTreeModel treeModel = new DefaultTreeModel(root);
+    MyToolWindow that = this;
+    ApplicationManager.getApplication().executeOnPooledThread(() -> {
+      Hashtable<String, Set<List<Encl_name_pos_tuple>>> result = Main.main(arguments);
 
-    Hashtable<String, Set<List<Encl_name_pos_tuple>>> result = Main.main(arguments);
+      Enumeration<String> res_to_analyze = result.keys();
+      while (res_to_analyze.hasMoreElements()) {
+        String key = res_to_analyze.nextElement();
+        Set<List<Encl_name_pos_tuple>> current_violation = result.get(key);
+        DefaultMutableTreeNode current_node = new DefaultMutableTreeNode(key);
+        root.add(current_node);
+        current_violation.forEach(v-> {
+          DefaultMutableTreeNode inner_node = new DefaultMutableTreeNode("Violation Path");
+          v.forEach(el->inner_node.add(new DefaultMutableTreeNode(el)));
+          current_node.add(inner_node);
+        });
+      }
 
-    Enumeration<String> res_to_analyze = result.keys();
-    while (res_to_analyze.hasMoreElements()) {
-      String key = res_to_analyze.nextElement();
-      Set<List<Encl_name_pos_tuple>> current_violation = result.get(key);
-      DefaultMutableTreeNode current_node = new DefaultMutableTreeNode(key);
-      root.add(current_node);
-      current_violation.forEach(v-> {
-        DefaultMutableTreeNode inner_node = new DefaultMutableTreeNode("Violation Path");
-        v.forEach(el->inner_node.add(new DefaultMutableTreeNode(el)));
-        current_node.add(inner_node);
-      });
-    }
-
-    filesInConnection.addTreeSelectionListener(this);
-
-    filesInConnection.setModel(treeModel);
-    filesInConnection.setRootVisible(true);
+      filesInConnection.addTreeSelectionListener(that);
+      filesInConnection.setModel(treeModel);
+      filesInConnection.setRootVisible(true);
+    });
 
 //    timeZone.setIcon(new ImageIcon(getClass().getResource("/toolWindow/Time-zone-icon.png")));
   }
@@ -79,6 +90,30 @@ public class MyToolWindow implements TreeSelectionListener {
 
   @Override
   public void valueChanged(TreeSelectionEvent e) {
+    try{
+      if (e.getNewLeadSelectionPath().getParentPath().getLastPathComponent().toString().equals("Violation Path")){
+        String[] target = e.getNewLeadSelectionPath().getLastPathComponent().toString().split(",");
+        String pos = target[target.length-1];
+        goToPos(target, pos);
+      }
+//    for logic below to work violated node must be in the same doc as the exception point "... at [...]"
+      else if(e.getNewLeadSelectionPath().getParentPath().getLastPathComponent().toString().equals("Possible Buffer Overflow Violations")){
+        String[] modTarget = ((DefaultMutableTreeNode)((DefaultMutableTreeNode)e.getNewLeadSelectionPath().getLastPathComponent()).getFirstChild()).getLastChild().toString().split(",");
+        String[] target = e.getNewLeadSelectionPath().getLastPathComponent().toString().split(" ");
+        String pos = target[target.length-1];
+        goToPos(modTarget, pos);
+      }
+    }
+    catch (Exception error){
+      System.out.println("Nothing to check");
+    }
+  }
 
+  private void goToPos(String[] target, String pos) {
+    Path file = Paths.get(target[target.length - 2]);
+    PsiFile targetFile = getFilesByName(activeProject,file.getFileName().toString(), GlobalSearchScope.allScope(activeProject))[0];
+    if(Paths.get(targetFile.getVirtualFile().getPath()).toString().equals(file.toString())){
+      new OpenFileDescriptor(activeProject,targetFile.getVirtualFile(), Objects.requireNonNull(targetFile.getViewProvider().getDocument()).getLineStartOffset(Integer.parseInt(pos)-1)).navigate(true);
+    }
   }
 }
